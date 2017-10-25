1053a43cfacaf0a5d62f1213fd544406b7a4d8e6
datta54 <dattatrayd@perscitussln.com>
2017-10-25 19:49:10 +0530

    First commit

---
+++ app/src/main/java/com/example/anttir3/webviewtest/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.anttir3.webviewtest;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView myWebView = (WebView) findViewById(R.id.webview);
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        myWebView.loadUrl("http://www.verifone.com");
+
+    }
+}
